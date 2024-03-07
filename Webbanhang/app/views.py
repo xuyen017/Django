0e55c3076b895a42655988b5786303260b4ab0c7
xuyen the <xuyen017>
2024-03-07 16:26:14 +0700

    Django day 2

---
+++ Webbanhang/app/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def home(request):
+    return render(request, 'app/home.html')
+def cart(request):
+    context={}
+    return render(request, 'app/cart.html', context)
+
+def checkout(request):
+    context={}
+    return render(request, 'app/checkout.html', context)
+
