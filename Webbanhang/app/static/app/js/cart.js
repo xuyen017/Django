a51a1af21298ce701b1cd5897bbf64e8f039465c
xuyen the <xuyen017>
2024-03-14 10:51:33 +0700

    demo 2

--- Webbanhang/app/static/app/js/cart.js
+++ Webbanhang/app/static/app/js/cart.js
@@ -8,7 +8,25 @@ for (i=0; i<updateBtns.length; i++){
         if (user === "AnonymousUser"){
             console.log('user not login')
         }else{
-            console.log('user login, success add')
+            updateUserOrder(productId, action)
         }
     })
+}
+function updateUserOrder(productId, action) {
+    console.log('da them')
+    var url = '/update_item/';
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'X-CSRFToken': csrftoken
+        },
+        body: JSON.stringify({ 'productId': productId, 'action': action })
+    })
+    .then((response) => {
+        return response.json();
+    })
+    .then((data) => {
+        console.log('data', data);
+    });
 }
--- Webbanhang/app/views.py
+++ Webbanhang/app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from .models import *
+import json
 
 # Create your views here.
 def home(request):
@@ -30,4 +31,19 @@ def checkout(request):
     
     context={'items': items,'order': order}
     return render(request, 'app/checkout.html', context)
-
+def updateItem(request):
+    data = json.loads(request.body)
+    productId = data['productId']
+    action = data['action']
+    customer = request.user.customer
+    product = Product.objects.get(id = productId)
+    order, created = Order.objects.get_or_create(customer =customer,complete =False)
+    orderItem, created = OrderItem.objects.get_or_create(order =order, product =product)
+    if action == 'add':
+        orderItem.quantity +=1
+    elif action == 'remove':
+        orderItem.quantity -=1
+    orderItem.save()
+    if orderItem.quantity <=0:
+        orderItem.delete()
+    return JsonResponse('added',safe=False)
